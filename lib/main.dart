465acfe97b5dc7e956d17bfff04a912eb2a7ea6b
vaginessa <vaginessa@users.noreply.github.com>
2023-06-14 07:40:45 +0000

    watchmdh

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
-var appName = "MyAppName";
-var url = "https://www.example.org/";
-var appColor = Colors.blue;
+var appName = "watchMDH";
+var url = "https://watchmdh.to/";
+var appColor = Colors.red;
 
 class MyApp extends StatelessWidget {
   WebViewController _controller;
